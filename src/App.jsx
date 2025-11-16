5de7ce23ccd0f49d64ae6d4256398bff81d89611
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-16 20:41:55 +0100

    import header file to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import image2 from '../src/assets/image2.png'
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <img src={image2} alt="  " />
     </div>
   )
