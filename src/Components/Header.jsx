b5ff8a9128a2e908546eddb767dc753fceecd9b0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-17 21:30:33 +0100

    updated header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -21,15 +21,19 @@ const Header = () => {
 
 
   
-    <div className='flex-col flex items-center '>
+    <div className='space-y-5 flex border-t gap-8 flex-col space-between  border-gray-100 left-0  right-0 top-25   bg-amber-400 items-center '>
+      <div>
+
+
+      </div>
        <ul className=' text-(--gray) font-semibold mr-20 '>
-        <li className='p-4 hover:text-black'>Home</li>
-        <li className='p-4 hover:text-black'>About</li>
-        <li className='p-4 hover:text-black'>Vision</li>
-        <li className='p-4 hover:text-black'>Work</li>
-        <li className='p-4 hover:text-black'>Team</li>
+        <a href='#home' className='p-4 hover:text-black'>Home</a>
+        <a href='#about' className='p-4 hover:text-black'>About</a >
+        <a href='#vsion'  className='p-4 hover:text-black'>Vision</a>
+        <a href='#work'  className='p-4 hover:text-black'>Work</a>
+        <a href='#team'  className='p-4 hover:text-black'>Team</a>
     </ul>     
-  <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full 
+  <button className='bg-(--purple) hover:bg-[#5f0df8cc]  rounded-full 
   cursor-pointer font-medium px-6 text-white py-3.5 text-center '>Partner with Us</button>
 
     </div>
