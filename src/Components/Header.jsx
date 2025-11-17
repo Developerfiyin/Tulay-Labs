8539dd3ad28bab6625aa38f95c4eaa3f878a3ca2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-17 20:37:15 +0100

    added ul for small screens

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,15 +7,33 @@ const Header = () => {
     <div className='w-90 m-4'>
     <img src={LOGO} alt="THE  LOGO IMAGE " />
     </div>
-    
-    <ul className='flex text-(--gray) font-semibold mr-20'>
+    <div className='hidden'>
+          <ul className='flex text-(--gray) font-semibold mr-20 '>
         <li className='p-4 hover:text-black'>Home</li>
         <li className='p-4 hover:text-black'>About</li>
         <li className='p-4 hover:text-black'>Vision</li>
         <li className='p-4 hover:text-black'>Work</li>
         <li className='p-4 hover:text-black'>Team</li>
     </ul>     
-  <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full cursor-pointer font-medium px-6 text-white py-3.5 text-center  mr-4'>Partner with Us</button>
+  <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full 
+  cursor-pointer font-medium px-6 text-white py-3.5 text-center  mr-4'>Partner with Us</button>
+    </div>
+
+
+  
+    <div className='flex-col flex items-center '>
+       <ul className=' text-(--gray) font-semibold mr-20 '>
+        <li className='p-4 hover:text-black'>Home</li>
+        <li className='p-4 hover:text-black'>About</li>
+        <li className='p-4 hover:text-black'>Vision</li>
+        <li className='p-4 hover:text-black'>Work</li>
+        <li className='p-4 hover:text-black'>Team</li>
+    </ul>     
+  <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full 
+  cursor-pointer font-medium px-6 text-white py-3.5 text-center '>Partner with Us</button>
+
+    </div>
+
   </section>
   )
   
