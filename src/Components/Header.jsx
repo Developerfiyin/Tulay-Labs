9cbaadfd099687b92295563ef9ae511aa2fbe396
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-17 19:57:11 +0100

    styled ul

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,19 +3,19 @@ import LOGO from '../assets/logo.png'
 
 const Header = () => {
   return (
-  <section className='bg-white py-13 mx-auto items-center flex justify-between '>
-    <div className='w-90'>
+  <section className='bg-white py-9 mt-0 px-4 items-center flex justify-between '>
+    <div className='w-90 m-4'>
     <img src={LOGO} alt="THE  LOGO IMAGE " />
     </div>
     
-    <ul className='flex gap-4'>
-        <li className='p-2'>Home</li>
-        <li>About</li>
-        <li>Vision</li>
-        <li>Work</li>
-        <li>Team</li>
+    <ul className='flex text-(--gray) font-semibold mr-20'>
+        <li className='p-4 hover:text-black'>Home</li>
+        <li className='p-4 hover:text-black'>About</li>
+        <li className='p-4 hover:text-black'>Vision</li>
+        <li className='p-4 hover:text-black'>Work</li>
+        <li className='p-4 hover:text-black'>Team</li>
     </ul>     
-  <button className='bg-(--purple) p-2'>Partner with Us</button>
+  <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full cursor-pointer font-semibold px-6 text-white py-3.5 text-center  mr-4'>Partner with Us</button>
   </section>
   )
   
