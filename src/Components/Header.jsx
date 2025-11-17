69b87cded3c187f094f3656695dc067348b59f00
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-17 22:04:30 +0100

    Updated header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,9 @@ import LOGO from '../assets/logo.png'
 
 const Header = () => {
   return (
-  <section className='bg-white py-9 mt-0 px-4 items-center flex justify-between '>
-    <div className='w-90 m-4'>
+  <section className=''>
+    <nav className='bg-white py-9 mt-0 px-4 items-center flex justify-between '>
+   <div className='w-90 m-4 '>
     <img src={LOGO} alt="THE  LOGO IMAGE " />
     </div>
     <div className='hidden'>
@@ -17,11 +18,13 @@ const Header = () => {
     </ul>     
   <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full 
   cursor-pointer font-medium px-6 text-white py-3.5 text-center  mr-4'>Partner with Us</button>
-    </div>
+   </div>
+    </nav>
+ 
 
 
   
-    <div className='space-y-5 flex border-t gap-8 flex-col space-between  border-gray-100 left-0  right-0 top-25   bg-amber-400 items-center '>
+    <div className='flex flex-col space-y-5'>
       <div>
 
 
