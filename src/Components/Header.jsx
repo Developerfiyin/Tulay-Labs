68ae76d67d79cd700adbd5b7886532241c42d682
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-17 17:58:50 +0100

    added  root styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,16 +3,19 @@ import LOGO from '../assets/logo.png'
 
 const Header = () => {
   return (
-  <section className='bg-white py-13 px-4 flex justify-between ' >
-    <img src={LOGO} alt="THE LOGO IMAGE " />
-    <ul>
-        <li>Home</li>
+  <section className='bg-white py-13 mx-auto items-center flex justify-between '>
+    <div className='w-90'>
+    <img src={LOGO} alt="THE  LOGO IMAGE " />
+    </div>
+    
+    <ul className='flex gap-4'>
+        <li className='p-2'>Home</li>
         <li>About</li>
         <li>Vision</li>
         <li>Work</li>
         <li>Team</li>
     </ul>     
-  <button className='bg-purple p-2  '>Partner with Us</button>
+  <button className='bg-(--purple) p-2'>Partner with Us</button>
   </section>
   )
   
