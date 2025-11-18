b1be9466fd4481b6d1f92fe90459fd85ebdc6434
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-18 12:27:41 +0100

    Updated header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <Header/>
-      <img src={image2} alt="  " />
+      
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LOGO from '../assets/logo.png'
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
 
 const Header = () => {
   return (
   <section className='py-6'>
     <nav className='bg-white mt-0 px-4 items-center flex justify-between '>
-   <div className='w-90 m-4 '>
-    <img src={LOGO} alt="THE  LOGO IMAGE " />
+   <div className='w-90 m-4'>
+    <img src={LOGO} alt="THE  LOGO IMAGE"/>
     </div>
     <div className='hidden'>
           <ul className='flex text-(--gray) font-semibold mr-20 '>
@@ -21,18 +23,20 @@ const Header = () => {
     </nav>
  
 
-  
-    <div className='flex flex-col text-(--gray) h-screen space-y-4 font-semi-bold bg-red-600 items-center'>
+
+    <div className='flex flex-col text-(--gray) h-[70vh] space-y-2 font-semibold bg-(--gray-200) items-center'>
         <a href='#home' className='p-4 hover:text-black'>Home</a>
         <a href='#about' className='p-4 hover:text-black'>About</a >
         <a href='#vision'  className='p-4 hover:text-black'>Vision</a>
         <a href='#work'  className='p-4 hover:text-black'>Work</a>
         <a href='#team'  className='p-4 hover:text-black'>Team</a>
-      
-  <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full cursor-pointer font-medium px-6 text-white py-3.5 text-center '>Partner with Us</button>
-
+      <button className='bg-(--purple) hover:bg-[#5f0df8cc] rounded-full cursor-pointer font-medium mx-9 w-150 p-3.5 mb-5 text-white text-center'>Partner with Us</button>
     </div>
 
+       <div> 
+        <HiOutlineMenuAlt3 size={30}/>
+        </div> 
+        
   </section>
   )
   
