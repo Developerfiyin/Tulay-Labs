8fd59a2fdea558965064a73712deea78d1d9d11e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-19 17:43:56 +0100

    styled the div background

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='bg-w py-12 px-4 '>
+        
+        <div  className='flex justify-between '>
+         <div className='flex flex-col '>
+          <h1 className='text-(--gray) text-5xl font-semibold'>
+                Tulay Labs is a hub built around <br />
+                around <br />
+                one simple idea:
+            </h1>
+            <h1 className='text-6xl font-semibold' >"Connection"</h1>
+            </div>
+           
+            
+             </div>
+
+
+        At</div>
   )
 }
 
