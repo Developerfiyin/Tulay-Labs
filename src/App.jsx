8deec0365af5ed359322cbf5b80b2d30789b5446
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-19 16:03:11 +0100

    Created About file

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import image2 from '../src/assets/image2.png'
 import Header from './Components/Header'
 import Hero  from './Components/Hero'
+import About from './Components/About'
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Hero/>
-      
+      <About/>
     </div>
   )
 }
