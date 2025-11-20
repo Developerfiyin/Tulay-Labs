c4e16bc355d33bee770c2ec21f3d5094dee79e52
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-20 22:29:39 +0100

    added a div

--- src/Components/Vision.jsx
+++ src/Components/Vision.jsx
@@ -17,10 +17,20 @@ const Vision = () => {
            <h1 className='text-(--neutral) font-bold text-5xl mb-15'>Is Crucial</h1>
              <div className=''>
           <img src={image1} alt="" className='w-full' />
-
              </div>
           </div>
 
+
+          <div className='flex flex-col w-[50%] rounded-lg'>
+             
+             <div className='bg-(--gray-200) py-4 px-2 rounded-lg '>
+               <h3 className='text-(--neutral) font-semibold text-3xl pr-15 '>
+          We're here to help African founders <br /> reach the world.</h3>
+            <p className='text-(--gray) font-semibold '> Web3 was meant to be global, but access isn‘t equal yet. There‘s money <br />
+             and knowledge flowing freely in some regions, while Africa holds incredible people and projects waiting to be seen. <br />
+             Tulay Labs exists to close that gap.</p>
+             </div>
+          </div>
 
         </div> {/* THE FLEXED DIV*/}
       
