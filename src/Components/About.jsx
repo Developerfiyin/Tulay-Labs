4176eca603f6c67f0a332175cf0352c5a5b0850b
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-20 06:52:25 +0100

    Added images

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -60,7 +60,7 @@ const About = () => {
 
               
 
-        At</div>
+        </div>
   )
 }
 
--- src/Components/Vision.jsx
+++ src/Components/Vision.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Vision = () => {
   return (
-    <div>Vision</div>
+    <div className="bg-whie py-12 px-4">
+        <div className='flex justify-between'>
+
+          <div className='bg-(--gray) flex-col'>
+
+          </div>
+
+
+        </div> {/* THE FLEXED DIV*/}
+      
+      Vision
+      
+      </div>
   )
 }
 
