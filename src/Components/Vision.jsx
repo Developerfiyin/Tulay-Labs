4c26e410c3b2c7d49cd225fad9ea4a44fa7d1457
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-20 07:45:03 +0100

    Created file section

--- src/Components/Vision.jsx
+++ src/Components/Vision.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import image1 from '../assets/image4.png'
 const Vision = () => {
   return (
     <div className="bg-whie py-12 px-4">
         <div className='flex justify-between'>
 
-          <div className='bg-(--gray) flex-col'>
-
+          <div className='bg-(--gray-200) flex-col flex  py-4 px-4 w-[40%] rounded-md'>
+            <div className='bg-(--yellow) w-3 h-3 rounded-full mb-9'></div>
+           <h1 className='text-(-neutral) font-bold text-5xl mb-2'>Our</h1>
+           <h1 className='text-(--purple) font-bold text-5xl mb-2'>Existence</h1>
+           <h1 className='text-(--neutral) font-bold text-5xl mb-15'>Is Crucial</h1>
+             
+          <img src={image1} alt="" className='w-full ' />
           </div>
 
 
