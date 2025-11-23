7276f077b7dbd777c41b607553ec7ca79a34b460
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-23 01:08:58 +0100

    updated work.jsx

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -8,15 +8,21 @@ const Work = () => {
   return (
     <section className='py-12 px-4 bg-(--gray-200)'>
         
-      <div className='flex mx-auto'>
+      <div className='flex '>
         <div className=''> 
         <h6 className='text-xl font-semibold text-(--gray)'>The Breakdown</h6>
         <div className='flex space-x-6'>
          <h2 className='text-(--neutral) text-6xl font-semibold'>What We Do</h2>
        <img src={image1} alt="THE ARROW IMAGE " className='mt-8 ml-9' />
         </div>
-       
+         </div>
         </div>
+
+        <div className=' mt-8 py-6 flex justify-evenly px-2 mx-auto ' >
+            
+          <div className='bg-(--natural) flex-col flex' >
+            <h3>Talent Hub</h3>
+          </div>
 
       </div>
 
@@ -24,7 +30,7 @@ const Work = () => {
 
 
         
-        Work</section>
+        </section>
   )
 }
 
