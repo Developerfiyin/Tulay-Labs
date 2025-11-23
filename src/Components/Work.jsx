66f2e0bf8f884285b7170e03a1329bbda306c1a0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-23 23:35:56 +0100

    updated work.jsx

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -18,10 +18,9 @@ const Work = () => {
          </div>
         </div>
 
-        <div className=' mt-8 py-6 flex flex-3 justify-evenly px-2' >
-            
-          <div className='bg-(--natural) flex-col p-5 flex mx-auto' >
-            <div className='flex justify-between'>
+        <div className=' mt-8 py-6 flex gap-4 justify-evenly px-2' >
+          <div className='bg-(--natural) flex-col p-5 gap-4 flex mx-auto' >
+          <div className='flex justify-between'>
            <h3 className='text-(--neutral) text-2xl font-semibold mr-15 mb-20'>Talent Hub</h3>
            <div className='flex gap-3'> 
              <div className='w-3 h-3 bg-(--purple-200) rounded-full'></div>
@@ -29,10 +28,35 @@ const Work = () => {
             <div className='w-3 h-3 rounded-full bg-(--purple-400) '></div>
            </div>
             </div>
+            <p className='font-semibold text-(--gray) mb-6' > Africa has incredible Web3 talent, from developers to marketers and community managers, yet much remains unseen and underused.</p>
+            <p className='text-(--gray) font-medium '>The challenge is access, not ability. Tulay Labs vets, equips, and connects African talent with global Web3 projects, helping startups, investors, and businesses discover and work with the skills Africa has to offer.</p>
+          </div>
 
-               <p className='font-semibold text-(--gray) mb-6' > Africa has incredible Web3 talent, from developers to marketers and community managers, yet much remains unseen and underused.</p>
-            <p className='text-(--gray) font-medium'>The challenge is access, not ability. Tulay Labs vets, equips, and connects African talent with global Web3 projects, helping startups, investors, and businesses discover and work with the skills Africa has to offer.</p>
+            
+        <div className='bg-(--natural) flex-col p-5 flex mx-auto' >
+          <div className='flex justify-between'>
+           <h3 className='text-(--neutral) text-2xl font-semibold mr-15 mb-20'>Talent Hub</h3>
+           <div className='flex gap-3'> 
+             <div className='w-3 h-3 bg-(--purple-200) rounded-full'></div>
+            <div className='w-3 h-3 rounded-full bg-(--purple-300)'></div>
+            <div className='w-3 h-3 rounded-full bg-(--purple-400) '></div>
+           </div>
+            </div>
+            <p className='font-semibold text-(--gray) mb-6' > Africa has incredible Web3 talent, from developers to marketers and community managers, yet much remains unseen and underused.</p>
+            <p className='text-(--gray) font-medium '>The challenge is access, not ability. Tulay Labs vets, equips, and connects African talent with global Web3 projects, helping startups, investors, and businesses discover and work with the skills Africa has to offer.</p>
+          </div>
 
+        <div className='bg-(--natural) flex-col p-5 flex mx-auto' >
+          <div className='flex justify-between'>
+           <h3 className='text-(--neutral) text-2xl font-semibold mr-15 mb-20'>Talent Hub</h3>
+           <div className='flex gap-3'> 
+             <div className='w-3 h-3 bg-(--purple-200) rounded-full'></div>
+            <div className='w-3 h-3 rounded-full bg-(--purple-300)'></div>
+            <div className='w-3 h-3 rounded-full bg-(--purple-400) '></div>
+           </div>
+            </div>
+            <p className='font-semibold text-(--gray) mb-6' > Africa has incredible Web3 talent, from developers to marketers and community managers, yet much remains unseen and underused.</p>
+            <p className='text-(--gray) font-medium '>The challenge is access, not ability. Tulay Labs vets, equips, and connects African talent with global Web3 projects, helping startups, investors, and businesses discover and work with the skills Africa has to offer.</p>
           </div>
 
       </div>
