f47954e75c79c49e9a236909c23dfb29baaf4176
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-24 14:41:31 +0100

    added newsletter section

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <section className='bg-white py-12 pl-2 mt-9 '>
+
+        <div className='flex justify-between gap-4'>
+       
+       <div>
+        <h1 className='font-medium text-5xl text-(-neutral) mb-8 '>We're Open to Partnerships & Collaborations</h1>
+        <p className='text-(--gray) font-semibold '>We promise to give you unrestricted access to high quality African Web3 opportunities and vetted startups.</p>
+       </div>
+
+       <div>
+        <h2 className='font-medium text-2xl  text-(--neutral) '>We can also send you email updates so you don't miss what's happening in our ecosystem.</h2>
+       </div>
+        </div>
+    </section>
+
   )
 }
 
