424b0c68d1846f1aa9447fca8076cbc88b458552
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-24 22:27:26 +0100

    Added a textarea and styled it.

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -9,12 +9,13 @@ const Newsletter = () => {
        <div className=''>
         <h1 className='font-medium text-5xl text-(--neutral) mb-8 '>We're Open to Partnerships & Collaborations</h1>
         <p className='text-(--gray) font-semibold '>We promise to give you unrestricted access to high quality African Web3 opportunities and vetted startups.</p>
-        <input type="text" className='bg-(--gray-200) outline-none focus:ring-2 focus:ring-(--purple) focus:outline-none rounded-lg mt-5 px-3 py-3  w-100 ' />
+        <input type="text" className='bg-(--gray-200) mb-4 focus:ring-2 focus:ring-(--purple) focus:outline-none rounded-lg mt-5 px-3 py-3 w-100'/>
+        <textarea name="" id="" className='bg-(--gray-200) w-100  focus:ring-2 focus:ring-(--purple) focus:outline-none px-3 py-3 resize-none rounded-lg'></textarea>
        </div>
 
        <div className='ml-12'>
         <h2 className='font-medium text-2xl text-(--neutral) '>We can also send you email updates so you don't miss what's happening in our ecosystem.</h2>
-        <input type="text" className='' />
+        <input type="text" className='bg-(--gray-200) rounded-2xl w-70 px-3 py-3 mt-4 focus:outline-none focus:ring-2 focus:ring-(--purple) ' />
        </div>
         </div>
     </section>
