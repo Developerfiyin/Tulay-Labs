4d0a415b47b02ca3f4ed8bb0ff2a4bcf656209b4
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-24 20:24:08 +0100

    updated Newsletter

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <section className='bg-white py-12 pl-2 mt-9 '>
+    <section className='bg-white py-12 ml-6 mt-9 '>
 
-        <div className='flex justify-between gap-4'>
+        <div className='lg:flex grid gap-4'>
        
        <div className=''>
-        <h1 className='font-medium text-5xl text-(-neutral) mb-8 '>We're Open to Partnerships & Collaborations</h1>
+        <h1 className='font-medium text-5xl text-(--neutral) mb-8 '>We're Open to Partnerships & Collaborations</h1>
         <p className='text-(--gray) font-semibold '>We promise to give you unrestricted access to high quality African Web3 opportunities and vetted startups.</p>
+        <input type="text" className='bg-(--gray-200) outline-none focus:ring-2 focus:ring-(--purple) focus:outline-none rounded-lg mt-5 px-3 py-3  w-100 ' />
        </div>
 
-       <div className='ml-9'>
+       <div className='ml-12'>
         <h2 className='font-medium text-2xl text-(--neutral) '>We can also send you email updates so you don't miss what's happening in our ecosystem.</h2>
         <input type="text" className='' />
        </div>
