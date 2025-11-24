f37ed022c8853b2074d7f297bc250a62890230f2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-24 23:15:10 +0100

    updated newsletter section

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -4,10 +4,10 @@ const Newsletter = () => {
   return (
     <section className='bg-white py-12 ml-6 mt-9 '>
 
-        <div className='lg:flex grid gap-4'>
+        <div className='lg:flex grid gap-5'>
        
        <div className=''>
-        <h1 className='font-medium text-5xl text-(--neutral) mb-8 '>We're Open to Partnerships & Collaborations</h1>
+        <h1 className='font-medium md:text-5xl text-3xl text-(--neutral) mb-8 '>We're Open to Partnerships & Collaborations</h1>
         <p className='text-(--gray) font-semibold '>We promise to give you unrestricted access to high quality African Web3 opportunities and vetted startups.</p>
         <input type="text"placeholder='Enter Your Email Here...'
          className='bg-(--gray-200) mb-4 focus:ring-2 focus:ring-(--purple) focus:outline-none rounded-lg mt-5 px-3 py-3 w-100'/>
