7b73817c4a79b3bfd5582816028596885aa3a2bb
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-25 16:24:50 +0100

    updated  team section

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import image17 from '../assets/image17.png'
-
+import image18 from '../assets/image18.png'
 
 const Team = () => {
   return (
-    <section className='bg-(--gray-200) text-center items-center justify-center py-12 px-4 '>
+    <section className='bg-(--gray-200) text-center items-center  py-12 px-4 '>
             <h2 className='text-3xl text-center mt-9 text-(--neutral) font-medium '> Meet the Growing Team </h2>
             <p className='font-medium text-(--gray) mb-8'> These are the passionate team of builders, thinkers, and dreamers
                  working together to unlock Africa‘s Web3 potential. </p>
 
-              <div className='flex justify-center '>
+              <div className='lg:flex gap-2 grid '>
  
-                   <div className='bg-white mx-auto py-4 px-4 rounded-lg'>
-                    <img src={image17} alt="BRO DAVID'S PICTURE" className='w-70 h-auto rounded-4xl py-2 px-4'/>
+                   <div className='bg-white py-4 px-4 rounded-lg'>
+                    <img src={image17} alt="BRO DAVID'S PICTURE" className='w-80  rounded-4xl py-2 px-4'/>
+                  <h2 className='text-(--neutral) font-semibold text-left pl-2 border-l-4 border-(--yellow) text-2xl'>David Adeola</h2>
+                   <p className='text-(--gray) font-semibold text-left pl-3 ' >Founder TulayLabs</p>
+                     
+                     <div className='flex gap-2 m '>
+                     <div className='w-3 h-3 bg-(--purple-200) rounded-full'></div>
+                    <div className='w-3 h-3 bg-(--purple-300) rounded-full'></div>
+                     <div className='w-3 h-3 bg-(--purple-400) rounded-full'></div>
+                     </div>
+
+
+                   </div>
+
+             <div className='bg-white mx-auto py-4 px-4 rounded-lg'>
+                    <img src={image18} alt="BRO DAVID'S PICTURE" className='w-70 h-auto rounded-4xl py-2 px-4'/>
                   <h2 className='text-(--neutral) font-semibold text-left pl-2 border-l-4 border-(--yellow) text-2xl'>David Adeola</h2>
                    </div>
 
-            
             </div>
 
         Team </section>
