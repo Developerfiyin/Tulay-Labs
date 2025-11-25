f8610fbf8907e86321d1167146cbbf7bf965517d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-25 22:29:39 +0100

    Created event section

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Event = () => {
   return (
-    <div>Event</div>
+    <section className='bg-white py-12 px-5 '>
+        <div className='flex gap-8'>
+          <div className='flex gap-2'>
+            <h2 className='text-3xl text-(--gray) font-semibold'> We've got interesting</h2>
+            <h2>Events</h2>
+            <h2>coming up</h2>
+          </div>
+
+
+        </div>
+        </section>
   )
 }
 
