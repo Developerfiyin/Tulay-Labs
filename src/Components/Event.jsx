dc5a131e69b4d56a56895b962ec9a9efbb77febc
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-26 03:13:38 +0100

    deleted unnecessary lines of code

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
-
+import image1 from '../assets/image20.png'
 const Event = () => {
   return (
     <section className='bg-white py-12 px-5 '>
-        <div className='lg:flex gap-8 '>
-          <div className='flex gap-2'>
-            <h2 className='lg:text-5xl text-2xl text-(--gray) font-semibold'> We've got interesting</h2>
-            <h2 className='lg:text-5xl text-2xl text-(--neutral) font-semibold'>Events</h2>
+        <div className='lg:flex justify-between grid gap-8 '>
+             <div className=' flex flex-col'>
+           <h1 className='lg:text-6xl text-2xl text-(--gray) font-normal'> We've got interesting</h1>
+           <div>
+               <h2 className='lg:text-7xl text-2xl text-(--neutral) font-semibold'>Events</h2>
             <h2 className='lg:text-5xl text-2xl font-semibold text-(--gray)'>coming up</h2>
-          </div>
+           </div>
+          
+             </div>
+           
+          
 
           <div className=''> 
-           <p className='text-(--gray) '>7th November</p>
+           <p className='text-(--gray)'>7th November</p>
            <h5 className='text-(neutral) font-medium'>Tulay Labs Mixer</h5>
            <h5 className=' text-(--gray) font-medium '>Bridging Africa's Web3 Talents With Global Opportunities</h5>
-           <a href="#post" className='hover:text-(--gray) text-(--purple) '>See post</a>
+           <a href="#post" className='hover:text-(--gray) text-(--purple-200) '>See post</a>
+           <img src={image1} alt=""  className='w-100 rounded-lg '/>
           </div>
 
         </div>
