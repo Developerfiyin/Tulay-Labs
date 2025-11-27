908c069bde7fef4b4e2e19f9f77900e338fe39c9
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-11-27 22:55:05 +0100

    updated  div style

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,19 +4,21 @@ const Footer = () => {
   return (
     <section className='py-12 px-4 bg-white '>
 
-      <div className='lg:flex flex-4  grid'>
+      <div className='lg:flex flex-4 gap-3 grid grid-cols-4'>
         
         <div >
-          <img src={LOGO} alt=" THE LOGO IMAGE " />
-          <p>We connect people, ideas and opportunities across borders. We help Africa’s builders, founders and communities find global access while bringing meaningful projects into Africa with trust and relevance.</p>
+          <img src={LOGO} alt=" THE LOGO IMAGE " className='mb-4 w-30' />
+          <p>We connect people, ideas and opportunities across borders. <br />
+           We help Africa’s builders, founders and communities find global <br />
+            access while bringing meaningful projects into Africa with trust and relevance.</p>
         </div>
 
-   <div>
-    <h3>Quick Links</h3>
-    <a href="#hero">Home</a>
-    <a href="#about">About</a>
-    <a href="">Works</a>
-    <a href="#">Team</a>
+   <div className='flex-col flex'>
+    <h3 className='text-base  text-(--neutral) font-medium mb-6 '>Quick Links</h3>
+    <a href="#hero" className='text-(--gray) hover:text-black p-2 font-semibold'>Home</a>
+    <a href="#about" className='text-(--gray) hover:text-black p-2 font-semibold'>About</a>
+    <a href="#work" className='text-(--gray) hover:text-black p-2 font-semibold'>Works</a>
+    <a href="#team " className='text-(--gray) hover:text-black p-2 font-semibold'>Team</a>
    </div>
       </div>
 
